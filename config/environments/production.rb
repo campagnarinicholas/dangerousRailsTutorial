16c14ba94ada6ab6152c1ef9102a053301eaa583
campagnarinicholas <campagnarinicholas@gmail.com>
2021-10-29 21:03:10 +0000

    a;sldkfj

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
     :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'timmoners@gmail.com',
+    :user_name      => 'timmoners',
     :password       => 'Chester#619',
     :domain         => 'gmail.com',
     :enable_starttls_auto => true
